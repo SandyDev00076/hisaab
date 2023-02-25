73bfe677f6e4267f4775157cc50abf31dedb68b7
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2023-02-26 02:28:52 +0530

    docs: add comment

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -3,6 +3,7 @@ import PocketBase from "pocketbase";
 const pbURL = import.meta.env.PB_URL;
 
 if (!pbURL) {
+  // environment variable missing
   throw new Error("Not able to fetch Pocket base URL");
 }
 
